c76bcb7a81ae5729bdc69849981355b7c0349172
Sérgio Carvalho <swedenland.me@gmail.com>
2026-06-30 19:39:06 -0300

    Create Login Page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './pages/Login.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Login />
 </template>
