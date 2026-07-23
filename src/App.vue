5df1d2f7dea1fe9b84b164c99a5bdce08081cead
Sérgio Carvalho <swedenland.me@gmail.com>
2026-07-22 21:48:37 -0300

    Implements a Navigation Route Stack Top Bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Header from './components/Header.vue';
+import { useAuthStore } from './stores/auth';
+
+const authStore = useAuthStore();
 </script>
 
 <template>
-  <RouterView />
+  <div>
+    <Header v-if="authStore.user && authStore.role !== 'denied'" />
+    <RouterView />
+  </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <header>
+    <button @click="router.back()">Voltar</button>
+    <h1 align="center">Gestabook</h1>
+  </header>
+</template>
